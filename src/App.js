aeea351e95c40e89fa0401ffdcb6c9f3a6e06e7b
Игорь Белодедов <igorbelodedov@MacBook-Pro.localdomain>
2023-03-11 12:24:26 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="scrollable">
+        {Array(100)
+          .fill()
+          .map((_, index) => (
+            <div
+              key={index}
+              style={{
+                height: "50px",
+                background:
+                  "#" + Math.floor(Math.random() * 16777215).toString(16),
+              }}
+            >
+              {"element: " + index}
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
